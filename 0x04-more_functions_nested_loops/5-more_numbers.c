6420f7af541a5ccc42042a3a7b55f431939d69e0
lorlahdey <horlorlahdey33@gmail.com>
2023-07-20 18:10:10 -0700

    C - More functions, more nested loops project

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -11,15 +11,15 @@ void more_numbers(void)
 
 	for (a = 0; a < 10; a++)
 	{
-		for (b = 0; b <= 14; b++)
-		{
-			if (b > 0)
-			{
-				_putchar((b / 10) + '0');
-			}
-			_putchar((b % 10) + '0');
-		}
-		_putchar('\n');
+	for (b = 0; b <= 14; b++)
+	{
+	if (b > 9)
+	{
+	_putchar((b / 10) + '0');
+	}
+	_putchar((b % 10) + '0');
+	}
+	_putchar('\n');
 	}
 
 }
