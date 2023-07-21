3b2dffa1ad3e32c53d11d287bb654cd6a48aab33
lorlahdey <horlorlahdey33@gmail.com>
2023-07-20 18:39:03 -0700

    C - More functions, more nested loops project

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,26 +11,26 @@ int main(void)
 
 	for (z = 1; z <= 100; z++)
 	{
-		if ((z % 3 == 0) && (z % 5 == 0))
-		{
-			printf("FizzBuzz");
-		}
-		else if (z % 3 == 0)
-		{
-			printf("Fizz");
-		}
-		else if (z % 5 == 0)
-		{
-			printf("Buzz");
-		}
-		else
-		{
-			printf("%d", z);
-		}
-		if (z != 100)
-		{
-			printf(" ");
-		}
+	if ((z % 3 == 0) && (z % 5 == 0))
+	{
+	printf("FizzBuzz");
+	}
+	else if (z % 3 == 0)
+	{
+	printf("Fizz");
+	}
+	else if (z % 5 == 0)
+	{
+		printf("Buzz");
+	}
+	else
+	{
+	printf("%d", z);
+	}
+	if (z != 100)
+	{
+	printf(" ");
+	}
 	}
 	return (0);
 }
